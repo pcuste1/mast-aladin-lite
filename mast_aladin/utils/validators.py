229cc31e7590b75bb5b256aeaebbb0850e92e45c
Patrick Custer <pcuster@stsci.edu>
2026-07-14 11:17:16 -0400

    remove try catch from validators

--- mast_aladin/utils/validators.py
+++ mast_aladin/utils/validators.py
@@ -2,9 +2,6 @@ from urllib.parse import urlparse
 
 
 def is_valid_s3_uri(uri: str) -> bool:
-    try:
-        parsed = urlparse(uri)
-        # Check scheme is 's3' and a bucket name exists in netloc
-        return parsed.scheme == 's3' and bool(parsed.netloc)
-    except Exception:
-        return False
+    parsed = urlparse(uri)
+    # Check scheme is 's3' and a bucket name exists in netloc
+    return parsed.scheme == 's3' and bool(parsed.netloc)
